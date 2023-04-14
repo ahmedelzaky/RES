188487f6a5718283afea98de56949320fc30e8c3
ahmed elzaki <ahmed.elzakistudy@gmail.com>
2023-04-14 14:22:23 +0200

    HOT FIX

--- Task_4/Task_4.ino
+++ Task_4/Task_4.ino
@@ -10,6 +10,10 @@ void loop() {
 
   for (i = 0; i < 256; i++) {
     analogWrite(led, i);
-    delay(20);
+    delay(10);
+  }
+  for (; i >= 0; i--) {
+    analogWrite(led, i);
+    delay(10);
   }
 }
