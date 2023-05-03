b403f43860a576aca662d8573a75d965bc809b63
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-05-03 20:39:37 +0200

    TASK_5

---
+++ Task_5/Binary_search/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[] = {1, 3, 7, 20, 36, 42, 50, 60, 180};
+    int size = sizeof(arr) /sizeof(arr[0]);
+    int num;
+    int index;
+
+    printf("Enter the number\n>>");
+    scanf("%d", &num);
+
+
+    index = binarySearch(arr, size, num);
+
+    if (index == -1)
+    {
+        printf("Can't Find The Number");
+    }
+    else
+    {
+        printf("Number found at position %d\n", index);
+    }
+    return 0;
+
+}
+
+int binarySearch(int arr[], int size, int num)
+{
+    int left = 0, right = size - 1, mid;
+
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+
+        if (arr[mid] == num)
+        {
+            return mid;
+        }
+        else if (arr[mid] < num)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
---
+++ Task_5/liner_serch/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[] = {20,1,3,60,50,42,180,36,7};
+    int num;
+
+
+    printf("Enter the number\n>>");
+    scanf("%d", &num);
+
+    for(int i = 0; i < sizeof(arr) /sizeof(arr[0]) ; i++)
+    {
+
+        if(num == arr[i])
+        {
+            printf("Number is found at position %d ", i);
+            return 0;
+        }
+
+
+    }
+    printf("Can't Find The Number");
+    return 1;
+
+}
